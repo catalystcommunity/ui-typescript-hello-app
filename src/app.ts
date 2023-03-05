f1c0018ae9a73d3c9db81ce5575421c012d516a6
Tod Hansmann <githubpub@todandlorna.com>
2023-03-04 17:44:20 -0700

    feat: start using webcomponents, add request pump

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,48 @@
-import './style.css'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>TypeScript</h1>
-    <p>I'm a real paragraph!</p>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
+function postMe(name, data, callback, onError) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState != 4 || request.status != 200) { return; }
+        var body = JSON.parse(request.responseText);
+        if (body.error) {
+            onError(body.error);
+        }
+        else {
+            callback(body);
+        }
+    };
+    request.open('POST', '/api/' + name, true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify(data));
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+class HelloItem extends HTMLElement {
+  constructor() {
+    super();
+    this.name = 'World'
+  }
+  static get observedAttributes() {
+    return ['name'];
+  }
+  attributeChangedCallback(property, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this[ property ] = newValue;
+  }
+  connectedCallback() {
+    
+    const shadow = this.attachShadow({ mode: 'closed' });
+
+    shadow.innerHTML = `<h1>Hello ${ this.name }!</h1>`;
+  }
+}
+
+class HelloApp extends HTMLElement {
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'closed' });
+    shadow.innerHTML = '<hello-item name="Caldo"></hello-item>';
+  }
+}
+
+customElements.define( 'hello-app', HelloApp );
+customElements.define( 'hello-item', HelloItem );
+
