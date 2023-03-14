a0ab684e983af8e3eec0fca6c0f559799ed36082
Tod Hansmann <githubpub@todandlorna.com>
2023-03-13 22:27:53 -0600

    feat: events testing is in there, and it is glorious

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,86 @@ function postMe(name:string, data:string, callback:Function, onError:Function) {
     request.send(JSON.stringify(data));
 }
 
+interface subscriptionCallback {
+  (eventName: string, data: object | string | Array<any>): void;
+}
+
+let events = {}
+function subscribeEvent(eventName: string, subscriber: string, callback:subscriptionCallback) {
+  let handler = (e: any) => {
+    callback(eventName, e.detail)
+  }
+  if (events[eventName] === undefined || events[eventName] === null) {
+    events[eventName] = {}
+  }
+  events[eventName][subscriber] = handler
+  document.addEventListener(eventName, handler)
+}
+
+function unsubscribeEvent(eventName: string, subscriber: string) :boolean {
+  let callback = events[eventName]?.[subscriber];
+  if (callback) {
+    document.removeEventListener(eventName, callback)
+    delete events[eventName][subscriber]
+    return true
+  }
+  return false
+}
+
+function fireEvent(eventName: string, data: object) {
+  document.dispatchEvent(new CustomEvent(eventName, {detail: data}))
+}
+
+function addListeners(num: number){
+  for(let i = 0; i < num; i++){
+    let listener = (eventName: string, foo: any) => {
+      // If all is well, no console.log, which is good because that's slow
+      if(eventName != 'amazingEvent') {
+        console.log(eventName)
+      }
+      if (foo.foo != 'bar') {
+        console.log(foo.foo)
+      }
+    }
+    subscribeEvent('amazingEvent', 'i:'+i, listener)
+  }
+}
+
+function removeListeners(num: number){
+  for(let i = 1; i < num; i++){
+    unsubscribeEvent('amazingEvent', 'i:'+i)
+  }
+  // Unsubscribe the same (first) event 4 times, it should be fine
+  unsubscribeEvent('amazingEvent', 'i:'+0)
+  unsubscribeEvent('amazingEvent', 'i:'+0)
+  unsubscribeEvent('amazingEvent', 'i:'+0)
+  let result = unsubscribeEvent('amazingEvent', 'i:'+0)
+  if (result) {
+    // this will never be seen
+    console.log('for some reason it succeeded removing the handler again')
+  }
+}
+function nowString(): string {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  return mm + '/' + dd + '/' + yyyy + " @ " + today.getHours() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+}
+
+function testEvents() {
+  console.log('adding listeners', nowString())
+  addListeners(40000);
+  console.log('firing event', nowString())
+  fireEvent("amazingEvent",{foo: 'bar'}) 
+  console.log('removing listeners', nowString())
+  removeListeners(3);
+  console.log('firing event', nowString())
+  fireEvent("amazingEvent",{foo: 'bar'}) 
+  console.log('finished event', nowString())
+}
+testEvents()
 
 function createLists(rows: number, fields: number, strings: number): Array<Array<any>> {
   let retVal: Array<Array<any>> = [];
